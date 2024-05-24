ba13b8022ccc07ca0dc04c327c9dfa0d972068d6
Erick Christian <erickchristian48@gmail.com>
2024-05-24 22:51:34 +0700

    feat: github action deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "ericc-ch.github.io",
   integrations: [mdx(), sitemap(), tailwind()],
 });
